1a9867e21709d89ff4389d891c57ed1a10b16fb0
saranf <qorwlsdk1995@naver.com>
2024-10-30 15:34:35 +0000

    compose test

--- webserver/src/scan_with_trivy.php
+++ webserver/src/scan_with_trivy.php
@@ -3,12 +3,15 @@
 $log_level = $_GET['log_level'] ?? 'INFO'; // 기본값은 INFO
 
 // 명령어 실행
-$command = "trivy --severity $log_level --format json --output trivy_report.json my-php-app:latest";
-exec($command, $output, $result_code);
+$command = "trivy image --severity $log_level --format json --output trivy_report.json my-php-app:latest";
+exec($command . ' 2>&1', $output, $result_code);
 
 if ($result_code === 0) {
     echo "Scan completed successfully. <a href='trivy_report.json'>Download JSON Report</a>";
 } else {
-    echo "Error during scan. Check log for details.";
+    echo "Error during scan. Check log for details.<br>";
+    echo "Command: $command<br>";
+    echo "Output:<br><pre>" . implode("\n", $output) . "</pre>";
 }
 ?>
+
